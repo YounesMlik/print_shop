0ef15861757c3464c03b715367757cee91c92180
Younes Mlik <younesmlik88@gmail.com>
2025-07-12 01:24:03 +0100

    add migrations

---
+++ database/migrations/2025_07_10_021033_create_option_option_attribute_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('option_option_attribute', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('option_id')->constrained()->onDelete('cascade');
+            $table->foreignId('option_attribute_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('option_option_attribute');
+    }
+};
