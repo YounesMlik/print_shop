02a3f02bf009694cbb675c48e68c9e88b60bae7e
Younes Mlik <younesmlik88@gmail.com>
2025-07-14 15:49:14 +0100

    fix option model fillable attributes

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Option extends Model
 {
-    protected $fillable = ['name', 'description'];
+    protected $fillable = ['name', 'description', 'product_id'];
 
     public function product()
     {
