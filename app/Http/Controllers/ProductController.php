85175c3f7c07b7cf0b998540e9011fc88b2b6fce
Younes Mlik <younesmlik88@gmail.com>
2025-07-26 04:11:05 +0100

    add tags to product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,18 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Tag;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::with('tags', 'category')->get();
+        $tagIds = $request->input('tags', []);
+
+        $products = Product::with('tags')
+            ->when(
+                !empty($tagIds),
+                fn($query) =>
+                $query->whereHas(
+                    'tags',
+                    fn($q) =>
+                    $q->whereIn('tags.id', $tagIds)
+                )
+            )
+            ->get();
 
         return Inertia::render('Products/Index', [
             'products' => $products,
+            'availableTags' => Tag::select('id', 'name')->get(),
+            'filters' => [
+                'tags' => Tag::whereIn('id', $tagIds)->get(['id as value', 'name as label']),
+            ],
         ]);
     }
+
 
     public function show(Product $product)
     {
