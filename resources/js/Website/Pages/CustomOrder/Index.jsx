e319fe689292d44f2fef402ba8acb419f7d4e75b
Younes Mlik <younesmlik88@gmail.com>
2025-07-28 16:40:24 +0100

    add custom order view

---
+++ resources/js/Website/Pages/CustomOrder/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Head } from '@inertiajs/inertia-react'
+
+export default function CustomOrderIndex() {
+
+  return (
+    <>
+      <Head title="Custom Order" />
+    </>
+  )
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use Inertia\Inertia;
 
 
 Route::resource('/products', ProductController::class)
     ->only(["index", "show"]);
+
+Route::get('/custom_order', function () {
+    return Inertia::render('CustomOrder/Index', []);
+});
