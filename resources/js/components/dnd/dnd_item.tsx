8ced3f3640eb3f92d9430a3f081e3682825a7a6d
Younes Mlik <younesmlik88@gmail.com>
2025-08-03 03:19:13 +0100

    style dnd item

--- resources/js/components/dnd/dnd_item.tsx
+++ resources/js/components/dnd/dnd_item.tsx
@@ -3,16 +3,23 @@ import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
 export function DndItem(props: { id: string; children: ReactNode }) {
-  const { attributes, listeners, transform, transition, setNodeRef } =
+  const { attributes, listeners, transform, transition, setNodeRef, isDragging } =
     useSortable({ id: props.id });
 
   const style = {
     transform: CSS.Translate.toString(transform),
     transition,
+    zIndex: isDragging ? 50 : 'auto',
+    position: isDragging ? 'relative' as const : 'static' as const, // z-index only works on positioned elements
   };
 
+  const className = "border border-white/30 dark:border-white/10 backdrop-blur-lg rounded-2xl shadow-xl p-6 bg-white/20 dark:bg-white/5" +
+    (isDragging
+      ? 'outline outline-2 outline-blue-400'
+      : '');
+
   return (
-    <div
+    <div className={className}
       ref={setNodeRef}
       style={style}
       {...attributes}
