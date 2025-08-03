81c39bb533546a7750e7f3167883437258967c77
Younes Mlik <younesmlik88@gmail.com>
2025-08-03 04:01:59 +0100

    style dnd item

--- resources/js/components/dnd/dnd_item.tsx
+++ resources/js/components/dnd/dnd_item.tsx
@@ -1,10 +1,18 @@
 import { type ReactNode } from "react";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import { GripVerticalIcon } from "lucide-react";
 
 export function DndItem(props: { id: string; children: ReactNode }) {
-  const { attributes, listeners, transform, transition, setNodeRef, isDragging } =
-    useSortable({ id: props.id });
+  const {
+    attributes,
+    listeners,
+    transform,
+    transition,
+    setNodeRef,
+    isDragging,
+
+  } = useSortable({ id: props.id });
 
   const style = {
     transform: CSS.Translate.toString(transform),
@@ -13,7 +21,7 @@ export function DndItem(props: { id: string; children: ReactNode }) {
     position: isDragging ? 'relative' as const : 'static' as const, // z-index only works on positioned elements
   };
 
-  const className = "border border-white/30 dark:border-white/10 backdrop-blur-lg rounded-2xl shadow-xl p-6 bg-white/20 dark:bg-white/5" +
+  const className = "border border-white/30 dark:border-white/10 backdrop-blur-lg rounded-2xl shadow-xl py-6 pl-6 bg-white/20 dark:bg-white/5" +
     (isDragging
       ? 'outline outline-2 outline-blue-400'
       : '');
@@ -22,11 +30,16 @@ export function DndItem(props: { id: string; children: ReactNode }) {
     <div className={className}
       ref={setNodeRef}
       style={style}
-      {...attributes}
-      {...listeners}
       aria-describedby="dnd"
     >
-      {props.children}
+      <div className="flex flex-row">
+        <div className="grow">
+          {props.children}
+        </div>
+        <button {...listeners} {...attributes}>
+          <GripVerticalIcon size={40} />
+        </button>
+      </div>
     </div>
   );
 }
