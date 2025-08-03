e0f8f5d92098d46fe5da0b3669a6c659706764b9
Younes Mlik <younesmlik88@gmail.com>
2025-08-03 02:07:26 +0100

    style form

--- resources/js/components/form_builder/form_interpreter.tsx
+++ resources/js/components/form_builder/form_interpreter.tsx
@@ -11,18 +11,8 @@ import { Button } from "@/components/ui/button";
 type FormBuilderSchema = Schema<typeof formBuilder>;
 
 export function FormInterpreter({ schema }: { schema: FormBuilderSchema }) {
-    /*
-    | We utilize the `useInterpreterStore` hook, which creates
-    | an interpreter store for us. This store is used for filling
-    | entities values based on a schema and builder definition.
-    */
     const interpreterStore = useInterpreterStore(formBuilder, schema, {
         events: {
-            /*
-            | We use the `onEntityValueUpdated` event callback
-            | to trigger an arbitrary entity validation every time
-            | its value is updated.
-            */
             onEntityValueUpdated(payload) {
                 void interpreterStore.validateEntityValue(payload.entityId);
             },
@@ -40,20 +30,21 @@ export function FormInterpreter({ schema }: { schema: FormBuilderSchema }) {
     }
 
     return (
-        <form onSubmit={(e) => {
-            e.preventDefault();
-            submitForm();
-        }}>
-            {/*
-      | We use the `InterpreterEntities` component to render the entities tree
-      | of the schema of our interpreter store. We pass the entity
-      | components for each defined entity type in our form builder
-      | (currently, it's only the text field).
-      */}
+        <form className="m-2"
+            onSubmit={(e) => {
+                e.preventDefault();
+                submitForm();
+            }}>
             <InterpreterEntities
                 interpreterStore={interpreterStore}
                 components={entity_components}
-            />
+            >
+                {({ children }) => (
+                    <div className="mb-4">
+                        {children}
+                    </div>
+                )}
+            </InterpreterEntities>
             <Button type="submit">Submit</Button>
         </form>
     );
