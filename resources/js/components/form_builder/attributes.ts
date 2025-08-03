b659e6b821301dbaa95b42383441d414751a60fe
Younes Mlik <younesmlik88@gmail.com>
2025-08-04 00:50:11 +0100

    add optionsAttribute

--- resources/js/components/form_builder/attributes.ts
+++ resources/js/components/form_builder/attributes.ts
@@ -7,3 +7,18 @@ export const labelAttribute = createAttribute({
         return z.string().min(1, "Label is required").parse(value);
     },
 });
+
+export const optionsAttribute = createAttribute({
+    name: "options",
+    validate(value) {
+        return z
+            .array(
+                z.object({
+                    label: z.string().min(1, "Label is required"),
+                    value: z.string().min(1, "Value is required"),
+                })
+            )
+            .min(1, "At least one option is required")
+            .parse(value);
+    },
+});
