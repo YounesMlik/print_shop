269641b26f3f73aac2711ed835678f9a3bc450a5
Younes Mlik <younesmlik88@gmail.com>
2025-08-07 18:07:57 +0100

    add category and super category to product page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -54,7 +54,7 @@ class ProductController extends Controller
 
     public function show(Product $product)
     {
-        $product->load(['options.optionAttributes', 'media']);
+        $product->load(['options.optionAttributes', 'media', 'category', 'category.superCategory']);
 
         return Inertia::render('Products/Show', [
             'product_resource' => $product->toResource(),
