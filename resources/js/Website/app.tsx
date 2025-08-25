8ab20cd6c09705102f1874443d7e0b16de5d6f8e
Younes Mlik <younesmlik88@gmail.com>
2025-08-25 03:37:09 +0100

    add i18n to inertia app

--- resources/js/Website/app.tsx
+++ resources/js/Website/app.tsx
@@ -1,8 +1,9 @@
-import * as React from 'react';
+import { StrictMode, Suspense, useEffect } from "react";
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { ThemeProvider } from "@/components/theme-provider";
 import Layout from '@/Layouts/Layout';
+import { setupI18n } from '@/components/i18n';
 
 createInertiaApp({
   resolve: name => {
@@ -20,12 +21,29 @@ createInertiaApp({
   setup({ el, App, props }) {
     const root = createRoot(el);
 
-    root.render(
-      <React.StrictMode>
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <App {...props} />
-        </ThemeProvider>
-      </React.StrictMode>
-    );
+    root.render(<Root App={App} props={props} />);
   },
 });
+
+
+function Root({ App, props }: any) {
+  const { locale, rtl } = props.initialPage.props as { locale: string; rtl: boolean };
+
+  // initialize i18next once with server locale
+  setupI18n(locale);
+
+  useEffect(() => {
+    document.documentElement.lang = locale;
+    document.documentElement.dir = rtl ? "rtl" : "ltr";
+  }, [locale, rtl]);
+
+  return (
+    <StrictMode>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Suspense fallback={<div className="p-4 text-sm opacity-60">Loading…</div>}>
+          <App {...props} />
+        </Suspense>
+      </ThemeProvider>
+    </StrictMode>
+  );
+}
