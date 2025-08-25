1015e9314bc6376f7fa5120aae07ac8fc5e6673f
Younes Mlik <younesmlik88@gmail.com>
2025-08-25 21:16:10 +0100

    refactor: simplify I18nProps type definition and update setupI18n function signature

--- resources/js/components/i18n.ts
+++ resources/js/components/i18n.ts
@@ -4,19 +4,17 @@ import ICU from "i18next-icu";
 import HttpBackend from "i18next-http-backend";
 
 type I18nProps = {
-    i18n: {
-        locale: string;
-        isRtl: boolean;
-        available: string[];
-        fallback: string;
-        assetsBase: string;
-        assetsVersion?: string | null;
-        defaultNS: string;
-        namespaces: string[];
-    };
+    locale: string;
+    isRtl: boolean;
+    available: string[];
+    fallback: string;
+    assetsBase: string;
+    assetsVersion?: string | null;
+    defaultNS: string;
+    namespaces: string[];
 };
 
-export function setupI18n(i18nProps: I18nProps['i18n']) {
+export function setupI18n(i18nProps: I18nProps) {
     const { locale, available, fallback, assetsBase, assetsVersion, defaultNS, namespaces } = i18nProps;
 
     if (i18n.isInitialized) {
