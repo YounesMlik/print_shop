21bcf872d2c5f028dcc2ffdba1a130761eb6c5a1
Younes Mlik <younesmlik88@gmail.com>
2025-08-25 21:58:51 +0100

    add logo to footer

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -21,9 +21,8 @@ export default function Layout({ children }) {
                     <div className="container mx-auto px-4">
                         <div className="flex flex-col items-center justify-between gap-4 text-sm text-muted-foreground md:flex-row">
                             <a href="#" className="inline-flex items-center gap-2 text-foreground">
-                                <div className="grid h-8 w-8 place-content-center rounded-xl bg-primary/10">
-                                    <Sparkles className="h-4 w-4 text-primary" />
-                                </div>
+                                
+                    <img src="/img/under_print_icon.svg" className="h-8 w-8 rounded-2xl bg-primary/10" />
                                 <span className="font-semibold tracking-tight">{import.meta.env.VITE_APP_NAME}</span>
                             </a>
                             <nav className="flex items-center gap-4">
