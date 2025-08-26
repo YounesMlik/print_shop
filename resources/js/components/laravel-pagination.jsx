cd57c605bdea36f52079ceb85033836ca017dcfc
Younes Mlik <younesmlik88@gmail.com>
2025-08-26 15:12:06 +0100

    add pagination translations

--- resources/js/components/laravel-pagination.jsx
+++ resources/js/components/laravel-pagination.jsx
@@ -8,6 +8,7 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from '@/components/ui/_pagination'
+import { useTranslation } from 'react-i18next';
 
 
 /**
@@ -17,6 +18,7 @@ export default function LaravelPagination({
   nav_data,
   onPageChange,
 }) {
+  const { t } = useTranslation();
   const is_on_first_page = nav_data.links[0].url !== null
   const is_on_last_page = nav_data.links[nav_data.links.length - 1].url !== null
   const links = nav_data.links.slice(1, -1)
@@ -24,34 +26,38 @@ export default function LaravelPagination({
   return (
     <Pagination>
       <PaginationContent>
-        <PreviousPage current_page={nav_data.current_page} disabled={is_on_first_page} onPageChange={onPageChange} />
+        <PreviousPage current_page={nav_data.current_page} disabled={is_on_first_page} onPageChange={onPageChange} >
+          {t("pagination.previous")}
+        </PreviousPage>
         <Pages links={links} onPageChange={onPageChange} />
-        <NextPage current_page={nav_data.current_page} disabled={is_on_last_page} onPageChange={onPageChange} />
+        <NextPage current_page={nav_data.current_page} disabled={is_on_last_page} onPageChange={onPageChange} >
+          {t("pagination.next")}
+        </NextPage>
       </PaginationContent>
     </Pagination>
   );
 }
 
 
-function PreviousPage({ disabled, current_page, onPageChange }) {
+function PreviousPage({ disabled, current_page, onPageChange, ...props }) {
   return (
     <PaginationItem>
       {disabled ? (
-        <PaginationPrevious className='cursor-pointer' onClick={() => onPageChange(current_page - 1)} />
+        <PaginationPrevious className='cursor-pointer' onClick={() => onPageChange(current_page - 1)} {...props} />
       ) : (
-        <PaginationPrevious disabled />
+        <PaginationPrevious disabled {...props} />
       )}
     </PaginationItem>
   );
 };
 
-function NextPage({ disabled, current_page, onPageChange }) {
+function NextPage({ disabled, current_page, onPageChange, ...props }) {
   return (
     <PaginationItem>
       {disabled ? (
-        <PaginationNext className='cursor-pointer' onClick={() => onPageChange(current_page + 1)} />
+        <PaginationNext className='cursor-pointer' onClick={() => onPageChange(current_page + 1)} {...props} />
       ) : (
-        <PaginationNext disabled />
+        <PaginationNext disabled {...props} />
       )}
     </PaginationItem>
   );
