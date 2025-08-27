635cc45b091bd28eabfcd17159db58863fae2e3f
Younes Mlik <younesmlik88@gmail.com>
2025-08-27 04:02:53 +0100

    fix helper by returning a normal array instead of a collection

--- app/helpers.php
+++ app/helpers.php
@@ -8,6 +8,6 @@ if (!function_exists('fakeLocalize')) {
         $locales = collect(config('locales.available', ['en']));
         return $locales->mapWithKeys(
             fn($lang) => [$lang => $value ? "$value $lang" : $value]
-        );
+        )->all();
     }
 }
