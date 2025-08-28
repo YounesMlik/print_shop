6e73ede31f8aabdd7276ec16f22822c3df353d52
Younes Mlik <younesmlik88@gmail.com>
2025-08-28 03:51:42 +0100

    add "php artisan schedule:work" to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -39,6 +39,7 @@ if [ "$APP_ENV" = "local" ]; then
     php_fpm php artisan migrate --seed
     php_fpm php artisan db:seed LocalSeeder
     php_fpm php artisan queue:work &
+    php_fpm php artisan schedule:work &
     php_fpm npm run dev &
     wait
     echo "deployment successful"
@@ -48,6 +49,7 @@ elif [ "$APP_ENV" = "production" ]; then
     shared_init
     php_fpm php artisan migrate --seed
     php_fpm php artisan queue:work &
+    php_fpm php artisan schedule:work &
     php_fpm npm run build &
     wait
     echo "deployment successful"
