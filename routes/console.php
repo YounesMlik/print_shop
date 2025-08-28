43720208898f666d449d8a164fcbc7709b8648e1
Younes Mlik <younesmlik88@gmail.com>
2025-08-28 04:08:59 +0100

    Schedule hourly backups

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('backup:run --only-db')->hourly();
+Schedule::command('backup:clean')->dailyAt('02:30');
+Schedule::command('backup:monitor')->dailyAt('03:00');
