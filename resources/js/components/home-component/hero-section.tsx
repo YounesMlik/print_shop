95f6cd5f05bb3d333a8c6dcec6829b81ccf2642e
Younes Mlik <younesmlik88@gmail.com>
2025-09-02 18:29:51 +0100

    move HeroSection out of HomePage

---
+++ resources/js/components/home-component/hero-section.tsx
@@ -0,0 +1,57 @@
+import { useTranslation } from "react-i18next";
+import { Head, Link, usePage } from "@inertiajs/react";
+import {
+  ChevronLeft,
+  ChevronRight,
+} from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+const hero_bg = "/public/img/home_page/hero_bg.jpg";
+
+
+export default function HeroSection({ className, super_categories, ...props }) {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <div className={cn(`bg-[url(${hero_bg})] text-shadow-lg/10`, className)} {...props}>
+      <div className="flex justify-between px-4 pb-4 bg-linear-to-r from-black/100 to-black/0">
+        <div className="flex flex-col gap-10 basis-63/128 text-muted-foreground hover:text-foreground transition-all text-start">
+          <Link href={route("products.index")} className="flex items-center">
+            {/* {i18n.dir() === "ltr" ? <ChevronLeft /> : <ChevronRight />} */}
+            <p className="text-4xl font-semibold tracking-tight sm:text-6xl md:text-7xl lg:text-8xl whitespace-pre-line">
+              {"Discover \nWhat's \nin \nStore"}
+            </p>
+          </Link>
+          <div className="flex flex-row flex-wrap gap-4 md:gap-0">
+            {super_categories.map((super_category, i) => (
+              <div className="basis-1/1 md:basis-1/2" key={i}>
+                <p className="text-lg">
+                  {super_category.name}
+                </p>
+                {super_category.categories.map((category, i) => (
+                  <p className="text-xs" key={i}>
+                    {category.name}
+                  </p>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex justify-center basis-2/128 self-center h-80 sm:h-90 md:h-100 lg:h-120">
+          <Separator orientation="vertical" />
+        </div>
+
+        <div className="flex flex-col gap-10 items-end basis-63/128 text-muted-foreground hover:text-foreground transition-all text-end">
+          <Link href={route("custom_order.index")} className="flex items-center gap-2">
+            <p className="text-4xl font-semibold tracking-tight sm:text-6xl md:text-7xl lg:text-8xl whitespace-pre-line">
+              {"Start \nYour \nCustom \nOrder"}
+            </p>
+            {/* {i18n.dir() === "ltr" ? <ChevronRight /> : <ChevronLeft />} */}
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
