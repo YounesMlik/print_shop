66eaa9c537eaca9e6280ed4e776cce51d4bbf289
Younes Mlik <younesmlik88@gmail.com>
2025-09-02 18:20:57 +0100

    style HeroSection

--- resources/js/Website/Pages/Home/Index.jsx
+++ resources/js/Website/Pages/Home/Index.jsx
@@ -10,6 +10,8 @@ import {
 } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
+import BaseLayout from "@/Layouts/BaseLayout";
+const hero_bg = "/public/img/home_page/hero_bg.jpg";
 
 export default function HomePage() {
   const { t } = useTranslation();
@@ -21,9 +23,10 @@ export default function HomePage() {
 
       {/* Hero */}
       <section className="relative">
+        <HeroSection super_categories={super_categories} />
+
         <div className="container mx-auto px-4">
 
-          <HeroSection super_categories={super_categories} />
 
           <div className="mx-auto max-w-4xl py-20 text-center md:py-28">
 
@@ -65,42 +68,47 @@ function HeroSection({ className, super_categories, ...props }) {
   const { t, i18n } = useTranslation();
 
   return (
-    <div className={cn("flex justify-between", className)} {...props}>
-      <div className="flex flex-col gap-10 basis-63/128 text-muted-foreground hover:text-foreground transition-all text-start">
-        <Link href={route("products.index")} className="flex items-center">
-          {/* {i18n.dir() === "ltr" ? <ChevronLeft /> : <ChevronRight />} */}
-          <p className="text-4xl tracking-tight sm:text-6xl md:text-7xl lg:text-8xl whitespace-pre-line">
-            {"Discover \nWhat's \nin \nStore"}
-          </p>
-        </Link>
-        <div className="flex flex-row flex-wrap gap-4 md:gap-0">
-          {super_categories.map((super_category) => (
-            <div className="basis-1/1 md:basis-1/2">
-              <p className="text-lg">
-                {super_category.name}
-              </p>
-              {super_category.categories.map((category) => (
-                <p className="text-xs">
-                  {category.name}
+    <div>
+      <div className={cn(`flex justify-between px-4 pb-4 bg-[url(${hero_bg})] text-shadow-lg/10`, className)} {...props}>
+        <div className="flex flex-col gap-10 basis-63/128 text-muted-foreground hover:text-foreground transition-all text-start">
+          <Link href={route("products.index")} className="flex items-center">
+            {/* {i18n.dir() === "ltr" ? <ChevronLeft /> : <ChevronRight />} */}
+            <p className="text-4xl font-semibold tracking-tight sm:text-6xl md:text-7xl lg:text-8xl whitespace-pre-line">
+              {"Discover \nWhat's \nin \nStore"}
+            </p>
+          </Link>
+          <div className="flex flex-row flex-wrap gap-4 md:gap-0">
+            {super_categories.map((super_category, i) => (
+              <div className="basis-1/1 md:basis-1/2" key={i}>
+                <p className="text-lg">
+                  {super_category.name}
                 </p>
-              ))}
-            </div>
-          ))}
+                {super_category.categories.map((category, i) => (
+                  <p className="text-xs" key={i}>
+                    {category.name}
+                  </p>
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
 
-      <div className="flex justify-center basis-2/128 self-center h-80 sm:h-90 md:h-100 lg:h-120">
-        <Separator orientation="vertical" />
-      </div>
+        <div className="flex justify-center basis-2/128 self-center h-80 sm:h-90 md:h-100 lg:h-120">
+          <Separator orientation="vertical" />
+        </div>
 
-      <div className="flex flex-col gap-10 items-end basis-63/128 text-muted-foreground hover:text-foreground transition-all text-end">
-        <Link href={route("custom_order.index")} className="flex items-center gap-2">
-          <p className="text-4xl tracking-tight sm:text-6xl md:text-7xl lg:text-8xl whitespace-pre-line">
-            {"Start \nYour \nCustom \nOrder"}
-          </p>
-          {/* {i18n.dir() === "ltr" ? <ChevronRight /> : <ChevronLeft />} */}
-        </Link>
+        <div className="flex flex-col gap-10 items-end basis-63/128 text-muted-foreground hover:text-foreground transition-all text-end">
+          <Link href={route("custom_order.index")} className="flex items-center gap-2">
+            <p className="text-4xl font-semibold tracking-tight sm:text-6xl md:text-7xl lg:text-8xl whitespace-pre-line">
+              {"Start \nYour \nCustom \nOrder"}
+            </p>
+            {/* {i18n.dir() === "ltr" ? <ChevronRight /> : <ChevronLeft />} */}
+          </Link>
+        </div>
       </div>
     </div>
   )
 }
+
+
+export const Layout = BaseLayout;
