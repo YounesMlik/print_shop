f2d96de7b2124a6eadead81ac6a37e73cbf132dc
Younes Mlik <younesmlik88@gmail.com>
2025-09-14 04:09:18 +0100

    remove backup recovery from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -51,7 +51,6 @@ if [ "$1" = "local" ]; then
     php_fpm cp .env.local .env
     php_fpm php artisan i18n:bump
     php_fpm php artisan key:generate
-    php_fpm php artisan backup:restore --backup=latest --connection=pgsql --reset --no-interaction
     php_fpm php artisan migrate
     php_fpm php artisan db:seed LocalSeeder
     php_fpm php artisan config:cache
@@ -66,7 +65,6 @@ elif [ "$1" = "production" ]; then
     php_fpm php artisan optimize
     php_fpm php artisan i18n:bump
     php_fpm php artisan key:generate
-    php_fpm php artisan backup:restore --backup=latest --connection=pgsql --reset --no-interaction
     php_fpm php artisan migrate --force
     php_fpm php artisan config:cache
     php_fpm npm run build
