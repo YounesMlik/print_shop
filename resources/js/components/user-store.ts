538b3de35c6834ae7601ca80661226c3337c1401
Younes Mlik <younesmlik88@gmail.com>
2025-09-20 18:06:20 +0100

    add setter to User

--- resources/js/components/user-store.ts
+++ resources/js/components/user-store.ts
@@ -1,10 +1,15 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import { makePersistable } from "mobx-persist-store";
+import { Writable, WritableKeysOf } from "type-fest";
 
 export class User {
     name: string = "";
     address: string = "";
     email?: string = undefined;
+
+    set<T extends object, K extends WritableKeysOf<T>>(key: K, value: T[K]) {
+        (this as any)[key] = value;
+    }
 
     constructor() {
         makeAutoObservable(this);
