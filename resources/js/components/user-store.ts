aeacd7fea70f46f144f0f25caa20db8a65911c8c
Younes Mlik <younesmlik88@gmail.com>
2025-09-21 14:49:42 +0100

    add setter helper to User

--- resources/js/components/user-store.ts
+++ resources/js/components/user-store.ts
@@ -1,21 +1,21 @@
+import { setter } from "@/components/helpers";
 import { makeAutoObservable, runInAction } from "mobx";
 import { makePersistable } from "mobx-persist-store";
-import { Writable, WritableKeysOf } from "type-fest";
 
-export class User {
+
+class User {
     name: string = "";
     address: string = "";
     email?: string = undefined;
 
-    set<T extends object, K extends WritableKeysOf<T>>(key: K, value: T[K]) {
-        (this as any)[key] = value;
-    }
+    readonly set = setter<this>;
 
     constructor() {
         makeAutoObservable(this);
     }
 }
 
+export { User };
 export const userStore = new User();
 
 makePersistable(userStore, {
