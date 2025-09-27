d75ecade44f42dc41b9cee694474c1f8d564a6e5
Younes Mlik <younesmlik88@gmail.com>
2025-09-27 17:48:51 +0100

    fix LaravelPagination types

--- resources/js/components/laravel-pagination.tsx
+++ resources/js/components/laravel-pagination.tsx
@@ -9,19 +9,24 @@ import {
   PaginationPrevious,
 } from '@/components/ui/_pagination'
 import { useTranslation } from 'react-i18next';
+import { NavData, NavEllipsis, NavLink, NavLinks } from '@/types/global';
 
 
+type LaravelPaginationProps = {
+  nav_data: NavData,
+  onPageChange: (page: number) => void,
+}
 /**
  * Works with laravel pagination data
  */
 export default function LaravelPagination({
   nav_data,
   onPageChange,
-}) {
+}: LaravelPaginationProps) {
   const { t } = useTranslation();
   const is_on_first_page = nav_data.links[0].url !== null
   const is_on_last_page = nav_data.links[nav_data.links.length - 1].url !== null
-  const links = nav_data.links.slice(1, -1)
+  const links = nav_data.links.slice(1, -1) as (NavLink | NavEllipsis)[]
 
   return (
     <Pagination>
@@ -39,31 +44,47 @@ export default function LaravelPagination({
 }
 
 
-function PreviousPage({ disabled, current_page, onPageChange, ...props }) {
+function PreviousPage(
+  { disabled, current_page, onPageChange, ...props }
+    : React.ComponentProps<typeof PaginationPrevious>
+    & {
+      disabled: boolean,
+      current_page: number,
+      onPageChange: (page: number) => void,
+    }
+) {
   return (
     <PaginationItem>
       {disabled ? (
         <PaginationPrevious className='cursor-pointer' onClick={() => onPageChange(current_page - 1)} {...props} />
       ) : (
-        <PaginationPrevious disabled {...props} />
+        <PaginationPrevious {...props} />
       )}
     </PaginationItem>
   );
 };
 
-function NextPage({ disabled, current_page, onPageChange, ...props }) {
+function NextPage(
+  { disabled, current_page, onPageChange, ...props }
+    : React.ComponentProps<typeof PaginationNext>
+    & {
+      disabled: boolean,
+      current_page: number,
+      onPageChange: (page: number) => void,
+    }
+) {
   return (
     <PaginationItem>
       {disabled ? (
         <PaginationNext className='cursor-pointer' onClick={() => onPageChange(current_page + 1)} {...props} />
       ) : (
-        <PaginationNext disabled {...props} />
+        <PaginationNext {...props} />
       )}
     </PaginationItem>
   );
 };
 
-function Pages({ links, onPageChange }) {
+function Pages({ links, onPageChange }: { links: (NavLink | NavEllipsis)[], onPageChange: (page: number) => void }) {
   return links
     .map((link, i) => (
       <PaginationItem key={i}>
