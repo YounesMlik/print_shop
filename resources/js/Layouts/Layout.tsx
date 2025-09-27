2c85e08a2af7792f49b149f644592a513c2f4aee
Younes Mlik <younesmlik88@gmail.com>
2025-09-27 18:11:33 +0100

    fix layouts type

--- resources/js/Layouts/Layout.tsx
+++ resources/js/Layouts/Layout.tsx
@@ -4,9 +4,10 @@ import { ScrollToTop } from '@/components/ui/_scroll-to-top'
 import { Separator } from '@/components/ui/separator'
 import BaseLayout from '@/Layouts/BaseLayout'
 import { ChevronUp, Info, Mail, Phone } from 'lucide-react'
+import { ReactNode } from 'react'
 import { useTranslation } from 'react-i18next'
 
-export default function Layout({ children }) {
+export default function Layout({ children }: { children: ReactNode }) {
     return (
         <BaseLayout>
             <div className="container mx-auto px-4 py-4">
