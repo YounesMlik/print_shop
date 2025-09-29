e669153e5866f6878cafb835dc75c662d23b4a5c
Younes Mlik <younesmlik88@gmail.com>
2025-09-29 15:51:47 +0100

    fix SuperCategoryShow types

--- resources/js/Website/Pages/SuperCategory/Show.tsx
+++ resources/js/Website/Pages/SuperCategory/Show.tsx
@@ -2,28 +2,37 @@ import { Head, Link, router } from '@inertiajs/react'
 import { useTranslation } from 'react-i18next'
 import { ProductsTagFilter } from '@/components/products-tag-filter';
 import { Breadcrumbs } from '@/components/breadcrumbs';
+import { Resource, ResourceCollection } from '@/types/global';
+import { SetRequired } from 'type-fest';
 
-export default function SuperCategoryShow({ products_collection, super_category_resource, available_tags, current_tags }) {
+
+type SuperCategoryShowProps = {
+  products_collection: ResourceCollection<SetRequired<Product, "images">>,
+  super_category_resource: Resource<SuperCategory>,
+  available_tags: ResourceCollection<Tag>,
+  current_tags: ResourceCollection<Tag>,
+}
+export default function SuperCategoryShow({ products_collection, super_category_resource, available_tags, current_tags }: SuperCategoryShowProps) {
   const { t } = useTranslation();
   const nav_data = products_collection.meta
   const products = products_collection.data
   const super_category = super_category_resource.data
-  available_tags = available_tags.data
-  current_tags = current_tags.data
+  const my_available_tags = available_tags.data
+  const my_current_tags = current_tags.data
 
-  function handleTagChange(tags) {
+  function handleTagChange(tags: Tag[]) {
     const tagIds = tags.map(tag => tag.id)
 
-    router.get(route(route().current(), { super_category: super_category.id }), { tags: tagIds }, {
+    router.get(route(route().current() as string, { super_category: super_category.id }), { tags: tagIds }, {
       preserveState: true,
       preserveScroll: true,
     })
   }
 
-  function handlePageChange(page, selectedTags) {
+  function handlePageChange(page: number, selectedTags: Tag[]) {
     const tagIds = selectedTags.map(t => t.id)
 
-    router.get(route(route().current(), { super_category: super_category.id }), { page, tags: tagIds }, {
+    router.get(route(route().current() as string, { super_category: super_category.id }), { page, tags: tagIds }, {
       preserveState: true,
       preserveScroll: true,
     })
@@ -46,8 +55,8 @@ export default function SuperCategoryShow({ products_collection, super_category_
 
         <ProductsTagFilter
           products={products}
-          available_tags={available_tags}
-          current_tags={current_tags}
+          available_tags={my_available_tags}
+          current_tags={my_current_tags}
           handleTagChange={handleTagChange}
           nav_data={nav_data}
           handlePageChange={handlePageChange}
