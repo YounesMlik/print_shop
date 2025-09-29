2982e853f57b2345f5fc9a2a005ab257d7595d1b
Younes Mlik <younesmlik88@gmail.com>
2025-09-29 15:40:50 +0100

    fix ProductsIndex types

--- resources/js/Website/Pages/Products/Index.tsx
+++ resources/js/Website/Pages/Products/Index.tsx
@@ -2,28 +2,36 @@ import { Head, router } from '@inertiajs/react'
 import { useTranslation } from 'react-i18next'
 import { ProductsTagFilter } from '@/components/products-tag-filter';
 import { Breadcrumbs } from '@/components/breadcrumbs';
+import { ResourceCollection } from '@/types/global';
+import { SetRequired } from 'type-fest';
 
-export default function ProductsIndex({ products_collection, available_tags, current_tags }) {
+
+type ProductsIndexProps = {
+  products_collection: ResourceCollection<SetRequired<Product, "images">>,
+  available_tags: ResourceCollection<Tag>,
+  current_tags: ResourceCollection<Tag>,
+}
+export default function ProductsIndex({ products_collection, available_tags, current_tags }: ProductsIndexProps) {
   const { t } = useTranslation();
   const nav_data = products_collection.meta
   const products = products_collection.data
-  available_tags = available_tags.data
-  current_tags = current_tags.data
+  const my_available_tags = available_tags.data
+  const my_current_tags = current_tags.data
 
-  function handleTagChange(tags) {
+  function handleTagChange(tags: Tag[]) {
     const tagIds = tags.map(tag => tag.id)
 
 
-    router.get(route(route().current()), { tags: tagIds }, {
+    router.get(route(route().current() as string), { tags: tagIds }, {
       preserveState: true,
       preserveScroll: true,
     })
   }
 
-  function handlePageChange(page, selectedTags) {
+  function handlePageChange(page: number, selectedTags: Tag[]) {
     const tagIds = selectedTags.map(t => t.id)
 
-    router.get(route(route().current()), { page, tags: tagIds }, {
+    router.get(route(route().current() as string), { page, tags: tagIds }, {
       preserveState: true,
       preserveScroll: true,
     })
@@ -41,8 +49,8 @@ export default function ProductsIndex({ products_collection, available_tags, cur
 
         <ProductsTagFilter
           products={products}
-          available_tags={available_tags}
-          current_tags={current_tags}
+          available_tags={my_available_tags}
+          current_tags={my_current_tags}
           handleTagChange={handleTagChange}
           nav_data={nav_data}
           handlePageChange={handlePageChange}
